3573a833aea8bbb466a9eba9621249def2e0ba19
imsamrat <imsamrat24@gmail.com>
2021-08-18 18:36:37 +0600

    I am samrat

---
+++ README.md
@@ -0,0 +1,1 @@
+# Project Name: Hospital Management System
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h2>Hospital Management System</h2>
+            <h2>Nayem</h2>
+            <h2>Samrat</h2>
+
+        </div>
+    );
+}
+
+export default App;
